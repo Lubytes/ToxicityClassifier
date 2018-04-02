6738c0e063586265b86e7fe864da56866f301614
Brandon Poole <PooleB@dal.ca>
2018-04-02 16:05:58 -0300

    Toxicity Random Forest
    - Complete random forest for toxicity

--- Random Forest Implementation/forest.py
+++ Random Forest Implementation/forest.py
@@ -4,6 +4,11 @@
 # Purpose:  Random Forest implementation with GloVe
 
 import csv
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+
 
 # Make dictionaries out of vectors
 dict = {}
@@ -22,6 +27,13 @@ for line in lines:
 sentenceToVector = []
 invalidKeys = []
 
+toxic = []          # Index 2
+severeToxic = []    # Index 3
+obscene = []        # Index 4
+threat = []         # Index 5
+insult = []         # Index 6
+identityHate = []   # Index 7
+
 with open('../Input Data/train-edited.csv', encoding="utf8", newline='') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
@@ -34,5 +46,30 @@ with open('../Input Data/train-edited.csv', encoding="utf8", newline='') as csvf
                 except:
                     invalidKeys.append(word)
             sentenceToVector.append([sum(x) for x in zip(*wordMappings)])
+            # Length of vectors should be 50
+            # We lose 262 cases from invalidy mapped GloVe vectors
+            if(len(sentenceToVector[-1]) != 50):
+                del sentenceToVector[-1]
+            else:
+                toxic.append(row[2])
+                severeToxic.append(row[3])
+                obscene.append(row[4])
+                threat.append(row[5])
+                insult.append(row[6])
+                identityHate.append(row[7])
         
+
+# === Random Forest Implementation ===
+
+#Model Selection splitter for evaluation
+X_train, X_test, y_train, y_test = train_test_split(sentenceToVector, toxic, test_size=0.33, random_state=42)
+
+clf = RandomForestClassifier(max_depth=2, random_state=0)
+clf.fit(X_train, y_train)
+
+# Evaluation
+
+testData = clf.predict(X_test)
+print(confusion_matrix(y_test, testData))
+
 
