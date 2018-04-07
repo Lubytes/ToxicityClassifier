05597b8a60d25ba0166433504c5848f1c647abd9
Brandon Poole <PooleB@dal.ca>
2018-04-07 17:18:41 -0300

    dataEvaluation Fix
    For some reason git didn't detect this one line???

--- Input Data/dataEvaluation.py
+++ Input Data/dataEvaluation.py
@@ -13,6 +13,7 @@ with open('train-edited.csv', encoding="utf8", newline='') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         row[1] = (row[1]).lower()
+        if "gay" in row[1]:
             if row[2] == "1" or row[3] == "1" or row[4] == "1" or row[5] == "1" or row[6] == "1" or row[7] == "1":
                 Toxic +=  1
             else:
